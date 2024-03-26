222eb4ef2fff25eb94375f6554a52425a210ab3d
muktpatel2711 <96394548+muktpatel2711@users.noreply.github.com>
2024-03-26 13:16:17 -0400

    MIS_First

---
+++ Cross_browser.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from pages import serachPerseon_page
+
+chrome_browser = webdriver.Chrome()
+fire_browser = webdriver.Firefox()
+edge_browser = webdriver.Edge()
+
+browsers = [chrome_browser,fire_browser,edge_browser]
+
+url = "https://dev.baps.dev/mis/"
+
+for browser in browsers:
+    browser.get(url)
+
+    browser.quit()

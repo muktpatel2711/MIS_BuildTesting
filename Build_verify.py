ff188cbcb12759f0458a5f4d579b5a36b60c43b4
muktpatel2711 <96394548+muktpatel2711@users.noreply.github.com>
2024-03-29 10:57:12 -0400

    Version name steps add

--- Build_verify.py
+++ Build_verify.py
@@ -1,5 +1,6 @@
 import time
 from telnetlib import EC
+import requests
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -7,13 +8,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
+def get_page_status_code(browser):
+    current_url = browser.current_url
+    response = requests.head(current_url)
+    return response.status_code
 # Chrome options for headless mode
 chrome_options = ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('window-size=1920x1080')
 #firefox_options = FirefoxOptions()
 #firefox_options.headless = True
 
@@ -34,24 +41,20 @@ for browser_name, browser in browsers:
     browser.find_element(By.XPATH,"//button[@class='btn btn-primary btn-submit']").click()
     browser.find_element(By.XPATH,"//button[text()='North America System Admin']").click()
     time.sleep(5)
-    hover_element = browser.find_element(By.XPATH, "// div[normalize - space() = 'V 1.0.11']")
-    # Wait for the version element to be visible
-    version_element = hover_element.text
+    version_element = browser.find_element(By.XPATH, "//div[@aria-describedby='cdk-describedby-message-0']")
+    ActionChains(browser).move_to_element(version_element).perform()
 
-    # If version element found, print its text
-    if version_element:
-        version_name = version_element
-        print(f"Version Name: {version_name}")
-    else:
-        print("Version name not found.")
-
-
+    # Execute JavaScript to retrieve the version number
+    version_name = browser.execute_script("return document.querySelector('.mat-tooltip').innerText;")
+    assert version_name == "BAPS MIS - V 1.0.11G"
+    True
+    print(version_name)
 
     chrome_browser.save_screenshot("screenshot.png")
     print("Screenshot saved successfully.")
 
-
-
+    status_code = get_page_status_code(chrome_browser)
+    print(f"HTTP Status Code: {status_code}")
 
 for _, browser in browsers:
     browser.quit()
